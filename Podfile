c58948ffd6f644d30894cae11dad31135bd8ca41
takumac <takumi.s2734@gmail.com>
2021-06-29 07:22:02 +0900

    アプリの設定画面の実装

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'Roulette' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
     pod 'Charts', '~>3.6.0'
+    pod 'Eureka', '~>5.3.3'  
   # Pods for Roulette
 
 end
--- Roulette/Controller/AppSettingVC.swift
+++ Roulette/Controller/AppSettingVC.swift
@@ -6,29 +6,65 @@
 //
 
 import UIKit
+import Eureka
 
-class AppSettingVC: UIViewController {
-    
-    @IBOutlet weak var tableView: UITableView!
+class AppSettingVC: FormViewController {
+    var type:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let testLabel: UILabel = UILabel();
-        testLabel.text = "Hello World!!!!";
-        testLabel.textColor = .red;
+        self.tableView.backgroundColor = Constants.commmonBackGroundColor
+        form
+            +++ Section("hogehoge")
+            <<< TextRow() { row in
+                row.title = "TextRow"
+                row.placeholder = "TextRow"
+            }
+            <<< PhoneRow() {
+                $0.title = "数値入力2"
+                $0.placeholder = "hohohohoho"
+            }
+            <<< IntRow() { row in
+                row.title = "IntRow"
+                row.placeholder = "IntRow"
+            }
+            <<< DateRow() {
+                $0.title = "DateRow"
+            }
+            <<< PushRow<String>() {
+                $0.title = "type"
+                $0.options = ["a","b","c","d"]
+                $0.value = "a"
+                $0.selectorTitle = "select type"
+                self.type = $0.value ?? "type"
+            }
+            <<< SegmentedRow<String>() {
+                $0.title = "ルーレット速度"
+                $0.options = ["slow", "normal", "fast"]
+                $0.value = "slow"
+            }
+            <<< SwitchRow() {
+                $0.title = "チートスイッチ"
+                $0.value = false
+            }.onChange({ row in
+                
+            })
+            
+            
+            +++ Section("foofoo")
+            <<< PhoneRow() {
+                $0.title = "foofoo"
+                $0.placeholder = "yoroshiku"
+            }
         
-        self.view.addSubview(testLabel);
-        testLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true;
-        testLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true;
-        testLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true;
-        testLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+            +++ ButtonRow("Button") { row in
+                row.tag = "delete_row"
+                row.title = "buttonrow"
+                
+            }
     }
-    
 
     /*
     // MARK: - Navigation
